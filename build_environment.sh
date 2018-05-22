0a02789ef52b0d1a42a9943b7b6474d42ad3e7d3
AlexsJones <tibbar@freedommail.ch>
2018-05-22 17:46:49 +0100

    updated

--- build_environment.sh
+++ build_environment.sh
@@ -3,7 +3,7 @@
 #     File Name           :     build_environment.sh
 #     Created By          :     jonesax
 #     Creation Date       :     [2017-09-26 21:26]
-#     Last Modified       :     [2017-09-27 13:53]
+#     Last Modified       :     [2018-05-22 17:46]
 #     Description         :
 #################################################################################
 
@@ -12,4 +12,4 @@ echo "Building for environment $1"
 rm -rf deployment || true
 
 
-vortex --template templates--output deployment -varpath ./environments/$1.yaml
+vortex --template templates --output deployment -varpath ./environments/$1.yaml
